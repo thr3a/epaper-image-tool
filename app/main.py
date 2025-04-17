315d93c1fbe52b50316e5abb4b36a8481493dc66
thr3a <12jk172@ms.dendai.ac.jp>
2025-04-18 01:57:33 +0900

    Initial commit

---
+++ app/main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+import os
+
+from .routers import items
+
+app = FastAPI()
+
+app.include_router(items.router)
+
+
+@app.get("/")
+async def root():
+    return {"message": "nyaa5"}
+
+
+@app.get("/health")
+async def health():
+    return {"status": "ok"}
+
+
+@app.get("/env")
+async def get_env():
+    """
+    環境変数をすべてJSON形式で出力するエンドポイント
+    """
+    return dict(os.environ)
